bbd7dd76fc060588558062b5a8ecf436226eb7b2
savasduzgun <savasdzgn@gmail.com>
2024-08-19 16:03:22 +0300

    EntityBase oluşturuldu.

---
+++ ChatGPTClone.Domain/Common/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatGPTClone.Domain.Common
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+
+    }
+}
