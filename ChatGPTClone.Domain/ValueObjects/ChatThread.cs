18eddb2aff25235c6aa7a8d37c6317be358560b4
savasduzgun <savasdzgn@gmail.com>
2024-08-20 00:11:17 +0300

    ChatThread oluşturuldu.

---
+++ ChatGPTClone.Domain/ValueObjects/ChatThread.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatGPTClone.Domain.ValueObjects
+{
+    public class ChatThread
+    {
+        public string Id { get; set; }
+        public List<ChatMessage> Messages { get; set; } = []; /*= new List<ChatMessage>();*/
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
