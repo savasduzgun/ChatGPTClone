d8e342984e8cbfb9ce703cde11aecb7b8a12c154
savasduzgun <savasdzgn@gmail.com>
2024-08-19 16:29:48 +0300

    AppUser implemente edildi.

--- ChatGPTClone.Domain/Identity/AppUser.cs
+++ ChatGPTClone.Domain/Identity/AppUser.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ChatGPTClone.Domain.Common;
+using Microsoft.AspNetCore.Identity;
 
 namespace ChatGPTClone.Domain.Identity
 {
-    public class AppUser : IdentityUser<Guid>
+    public class AppUser : IdentityUser<Guid>, IEntity, ICreatedByEntity, IModifiedByEntitiy
     {
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTimeOffset? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
     }
 }
