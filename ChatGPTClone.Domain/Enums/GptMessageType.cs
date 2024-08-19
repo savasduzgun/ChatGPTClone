61e322b1d96f06c05f59fc44748a52db23dda316
savasduzgun <savasdzgn@gmail.com>
2024-08-19 21:17:11 +0300

    GptMessageType enum eklendi.

---
+++ ChatGPTClone.Domain/Enums/GptMessageType.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatGPTClone.Domain.Enums
+{
+    public enum GptMessageType
+    {
+        // The prompt which is sent to the GPT model
+        System = 1,
+
+        // The mesaage which is sent by the user
+        User = 2,
+
+        // The mesaage which is sent by the assistant
+        Assistant = 3,
+    }
+}
