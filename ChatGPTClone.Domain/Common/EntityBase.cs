dd58c116fc6483aaad107d35feb8e75a668a2eb6
savasduzgun <savasdzgn@gmail.com>
2024-08-19 16:09:24 +0300

    IEntity ve ICreatedByEntity oluşturuldu.

--- ChatGPTClone.Domain/Common/EntityBase.cs
+++ ChatGPTClone.Domain/Common/EntityBase.cs
@@ -3,8 +3,6 @@
     public class EntityBase
     {
         public Guid Id { get; set; }
-        public DateTimeOffset CreatedOn { get; set; }
-        public string CreatedByUserId { get; set; }
         public DateTimeOffset? ModifiedOn { get; set; }
         public string? ModifiedByUserId { get; set; }
 
