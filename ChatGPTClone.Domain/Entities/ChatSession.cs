416952fa9fa3771c186c5aa0212a852ea18ba807
savasduzgun <savasdzgn@gmail.com>
2024-08-20 00:05:50 +0300

    AppUser ve ChatSession ilişkisi kuruldu.

--- ChatGPTClone.Domain/Entities/ChatSession.cs
+++ ChatGPTClone.Domain/Entities/ChatSession.cs
@@ -1,9 +1,16 @@
 ﻿using ChatGPTClone.Domain.Common;
+using ChatGPTClone.Domain.Enums;
+using ChatGPTClone.Domain.Identity;
+using ChatGPTClone.Domain.ValueObjects;
 
 namespace ChatGPTClone.Domain.Entities
 {
     public sealed class ChatSession : EntityBase
     {
         public string Title { get; set; }
+        public GptModelType ModelType { get; set; }
+        public List<ChatMessage> Messages { get; set; } = [];
+        public Guid AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
